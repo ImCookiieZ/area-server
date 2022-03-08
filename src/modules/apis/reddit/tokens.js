79ec25ddd80e4d48f285a3eb85ef5821d3a22cea
ImCookiieZ <53428372+ImCookiieZ@users.noreply.github.com>
2022-03-08 19:47:04 +0100

    reddit subscribe

--- src/modules/apis/reddit/tokens.js
+++ src/modules/apis/reddit/tokens.js
@@ -5,7 +5,7 @@ import { checkInputBeforeSqlQuery, createErrorMessage } from '../../Helper.js';
 
 import db_adm_conn from "../../db/index.js";
 export const client_id = (await db_adm_conn.query(`SELECT client_id FROM services WHERE service_name = 'reddit'`)).rows[0].client_id;
-export const client_secret = (await db_adm_conn.query(`SELECT client_id FROM services WHERE service_name = 'reddit'`)).rows[0].client_secret;
+export const client_secret = (await db_adm_conn.query(`SELECT client_secret FROM services WHERE service_name = 'reddit'`)).rows[0].client_secret;
 const redirect_uri = 'https://karl-area-server.herokuapp.com/reddit/callback';
 
 export const unsubscribe = async (req, res) => {
