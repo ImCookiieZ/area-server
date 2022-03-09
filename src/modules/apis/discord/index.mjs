8f7795f8ce08bb8ea0a575bd790d0f6a11a7ad15
marcel <marcel.taubert@epitech.eu>
2022-03-09 13:04:13 +0100

    Docs for spotify, twitch & dicord tokens

--- src/modules/apis/discord/index.mjs
+++ src/modules/apis/discord/index.mjs
@@ -4,13 +4,83 @@ import { secureRouteMiddleware, secureCallback } from '../../../middleware/secur
 
 const router = Router();
 
+/**
+ * @swagger
+ * /discord/subscribe:
+ *   get:
+ *     summary: Subscribe to discord
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Missing access token
+ */
 router.get('/discord/subscribe', secureRouteMiddleware, login);
 router.get('/discord/subscribe/app', secureRouteMiddleware, loginApp);
+
+/**
+ * @swagger
+ * /discord/unsubscribe:
+ *   get:
+ *     summary: Unsubscribe from discord
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ */
 router.get('/discord/unsubscribe', secureRouteMiddleware, unsubscribe);
 
-
+/**
+ * @swagger
+ * /discord/callback:
+ *   get:
+ *     summary: callback to store tokens
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad request
+ *       403:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/discord/callback', secureCallback, store_tokens);
 
+/**
+ * @swagger
+ * /discord/refresh:
+ *   get:
+ *     summary: Subscribe to discord
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Missing access token
+ */
 router.get('/discord/refresh', secureRouteMiddleware, refresh);
 
 export default router;
--- src/modules/apis/twitch/index.mjs
+++ src/modules/apis/twitch/index.mjs
@@ -6,13 +6,83 @@ import { secureRouteMiddleware, secureCallback } from '../../../middleware/secur
 
 const router = Router();
 
+/**
+ * @swagger
+ * /twitch/subscribe:
+ *   get:
+ *     summary: Subscribe to twitch
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Missing access token
+ */
 router.get('/twitch/subscribe', secureRouteMiddleware, login);
 router.get('/twitch/subscribe/App', secureRouteMiddleware, loginApp);
+
+/**
+ * @swagger
+ * /twitch/unsubscribe:
+ *   get:
+ *     summary: Unsubscribe from twitch
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ */
 router.get('/twitch/unsubscribe', secureRouteMiddleware, unsubscribe);
 
-
+/**
+ * @swagger
+ * /twitch/callback:
+ *   get:
+ *     summary: callback to store tokens
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad request
+ *       403:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/twitch/callback', secureCallback, store_tokens);
 
+/**
+ * @swagger
+ * /twitch/refresh:
+ *   get:
+ *     summary: Subscribe to twitch
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Missing access token
+ */
 router.get('/twitch/refresh', secureRouteMiddleware, refresh);
 
 export default router;
