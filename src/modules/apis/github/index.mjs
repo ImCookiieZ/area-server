46bbd493708e35dc09b427d568d0b187572aa9ca
marcel <marcel.taubert@epitech.eu>
2022-03-09 11:32:09 +0100

    Docs for reddit & github tokens

--- src/modules/apis/github/index.mjs
+++ src/modules/apis/github/index.mjs
@@ -5,13 +5,83 @@ import { secureRouteMiddleware, secureCallback } from '../../../middleware/secur
 
 const router = Router();
 
+/**
+ * @swagger
+ * /github/subscribe:
+ *   get:
+ *     summary: Subscribe to github
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Missing access token
+ */
 router.get('/github/subscribe', secureRouteMiddleware, login);
 router.get('/github/subscribe/app', secureRouteMiddleware, loginApp);
+
+/**
+ * @swagger
+ * /github/unsubscribe:
+ *   get:
+ *     summary: Unsubscribe from github
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ */
 router.get('/github/unsubscribe', secureRouteMiddleware, unsubscribe);
 
-
+/**
+ * @swagger
+ * /github/callback:
+ *   get:
+ *     summary: callback to store tokens
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad request
+ *       403:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/github/callback', secureCallback, store_tokens);
 
+/**
+ * @swagger
+ * /github/refresh:
+ *   get:
+ *     summary: refresh tokens
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Missing access token
+ */
 router.get('/github/refresh', secureRouteMiddleware, login);
 
 //router.get('/github/updateOnGithubPush', secureRouteMiddleware, createGithubPushTrigger);
--- src/modules/apis/reddit/index.mjs
+++ src/modules/apis/reddit/index.mjs
@@ -7,11 +7,83 @@ import { secureRouteMiddleware, secureCallback } from '../../../middleware/secur
 
 const router = Router();
 
+/**
+ * @swagger
+ * /reddit/subscribe:
+ *   get:
+ *     summary: Subscribe to reddit
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Missing access token
+ */
 router.get('/reddit/subscribe', secureRouteMiddleware, login);
 router.get('/reddit/subscribe/app', secureRouteMiddleware, loginApp);
+
+/**
+ * @swagger
+ * /reddit/unsubscribe:
+ *   get:
+ *     summary: Unsubscribe from reddit
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ */
 router.get('/reddit/unsubscribe', secureRouteMiddleware, unsubscribe);
 
+/**
+ * @swagger
+ * /reddit/callback:
+ *   get:
+ *     summary: callback to store tokens
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad request
+ *       403:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/reddit/callback', secureCallback, store_tokens);
+
+/**
+ * @swagger
+ * /reddit/refresh:
+ *   get:
+ *     summary: refresh tokens
+ *     parameters:
+ *          - in: cookie
+ *            name: token
+ *            type: JWT
+ *            required: true
+ *            description: JWT user got on login
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Missing access token
+ */
 router.get('/reddit/refresh', secureRouteMiddleware, refresh);
 
 //router.get('/reddit/updateOnSubreddit', secureRouteMiddleware, createLastPostOnSubreddit)
